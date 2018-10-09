2a2f2e6fc7e4a75f612b65df5749d946a75720e7
Patrick Ball <pball@hrdag.org>
2018-10-09 16:33:49 -0700

    task1a comments added

--- task1a/src/gennums.R
+++ task1a/src/gennums.R
@@ -1,12 +1,15 @@
-#!/usr/bin/env Rscript --vanilla 
-# 
+#!/usr/bin/env Rscript --vanilla
+#
 #  Author: PB
 #  Maintainer: PB
 #  License: (c) HRDAG 2018, some rights reserved: GPL v2 or newer
-# 
+#
 # generate and output 1000 random integers
-# the execution context is always the task 
+# the execution context is always the task
 
+# alternatively we could source a yaml-processing script here, like
+## source('../share/src/library.R')
+## paths <- getpaths()
 require(yaml)
 paths <- read_yaml('hand/paths.yaml')
 
@@ -15,4 +18,4 @@ y <- round(abs(rpois(1000, 1.1)) * 1000)
 d <- data.frame(c1=x, c2=y, row.names=NULL)
 write.table(d, paths$output_task1a, sep="|", row.names=FALSE)
 
-# done. 
+# done.
