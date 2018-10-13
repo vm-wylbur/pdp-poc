de5c4be7a301f41f0e2230fd9558e28a6bfc338b
Patrick Ball <pball@hrdag.org>
2018-10-12 21:34:55 -0700

    task4 shows full Makefile + argparse, with default checking

--- task4/src/task4.R
+++ task4/src/task4.R
@@ -9,7 +9,48 @@
 # -----------------------------------------------------------
 
 library(argparse)
+library(tools)
+library(rstudioapi)
 suppressPackageStartupMessages(library(tidyverse, quietly=TRUE))
+
+
+get_running_name <- function() {
+  if (interactive()) {
+    fname <- rstudioapi::getActiveDocumentContext()$path
+  } else {
+    # via commandArgs, look for file=
+    cmdArgs = commandArgs(trailingOnly = FALSE)
+    needle <- '--file='
+    fname <- grep(needle, cmdArgs, value=TRUE)
+    print(fname)
+  }
+  if (length(fname) > 0) {
+    fname <- basename(fname)
+    return(tools::file_path_sans_ext(basename(fname)))
+  }
+  return(FALSE)
+}
+
+default_checker <- function(args, python_code) {
+  for (line in python_code) { 
+    key <- str_sub(str_extract(line, '--[^,]+'), 3, -2)
+    if (is.na(key)) next
+    pth <- str_sub(str_extract(line, 'default=[\'"].*[\'"]'), 10, -2)
+    stopifnot(args[[key]] == pth)
+  }
+}
+
+chkargs <- function(args, parser, addlog=TRUE) {
+  if (addlog & !"log" %in% names(args)) {
+    fname <- get_running_name()
+    args[["log"]] <- str_glue('output/{fname}.log')
+  }
+  if (!interactive()) {
+    default_checker(args, parser$python_code)
+  }
+  return(args)
+}
+
 
 # get the args from the Makefile; of course we can move the python to a package
 # and of course we can hide this function in a common script
@@ -18,25 +59,20 @@ get_args <- function() {
   parser$add_argument("--arg1a", type="character", default="../task1a/output/task1a.csv")
   parser$add_argument("--arg1b", type="character", default="../task1b/output/task1b.csv")
   parser$add_argument("--output", type="character", default="cache/task4.csv")
-  p_tmp <- parser
   args <- parser$parse_args()
-  # we could add logic here:
-  # -- check that if run as a script, that the arguments passed match the defaults
-  # -- add a log argument, always.
+  args <- chkargs(args, parser)
   return(args)
 }
 
+
+#---main-----
 args <- get_args()
 print(str(args))
-sink(args$log)
 
-# print(args)
+sink(args$log)
 
 arg1a <- read_delim(args$arg1a, delim="|", col_types=cols())
 arg1b <- read_delim(args$arg1b, delim="|", col_types=cols())
-
-# glimpse(arg1a)
-# glimpse(arg1b)
 
 result <- bind_cols(
   select(arg1b, name),
